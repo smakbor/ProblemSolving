0da302a82166f564d6548cfeca78f65d3128f6e2
smakbor <smakbar3965@gmail.com>
2023-02-24 23:01:14 +0600

    find maximum and minimum number from an array

---
+++ main.js
@@ -0,0 +1,68 @@
+const object = {
+  name: "SM Akbor",
+  age: 24,
+  country: "Bangladesh",
+};
+
+// const convertArrayFromAnObject = (obj) => {
+//   return Object.keys(obj).map((key) => [key, obj[key]]);
+// };
+// console.log(convertArrayFromAnObject(object));
+// const convertArrayFromAnObject = (obj) => {
+//   return Object.keys(obj).map((key) => [key, obj[key]]);
+// };
+// console.log(convertArrayFromAnObject(object));
+
+// class Student {
+//   constructor(name, gpa) {
+//     this.name = name;
+//     this.gpa = gpa;
+//   }
+
+//   marks() {
+//     return `${this.name} is gained ${this.gpa} marks!`;
+//   }
+// }
+// const tamim = new Student("Tamim", 34);
+// console.log(tamim.marks());
+
+// find max number from three integer number
+
+// const findMaxNumber = (a, b, c) => {
+//   if (a > b && a > c) {
+//     return a;
+//   } else if (b > a && b > c) {
+//     return b;
+//   } else {
+//     return c;
+//   }
+// };
+
+// console.log(findMaxNumber(2, 4, 7));
+
+// find max number from an array
+
+// const findMaxNumberFromArray = (arr) => {
+//   let max = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// };
+// console.log(findMaxNumberFromArray([2, 3, 76, 4, 90, 5, 61, 100]));
+
+// find min number from an array
+// function findMinimumNumberFromArray(arr) {
+//   let len = arr.length;
+//   let minimum = Infinity;
+//   while (len--) {
+//     if (arr[len] < minimum) {
+//       minimum = arr[len];
+//     }
+//   }
+//   return minimum;
+// }
+
+// console.log(findMinimumNumberFromArray([2, 3, 76, 4, 90, 5, 61]));
